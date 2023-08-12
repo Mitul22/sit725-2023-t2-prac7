b1188dfc29c0fd8a795dacc1c466dca4c6a26c93
Mitul Tandon <53808940+Mitul22@users.noreply.github.com>
2023-08-12 19:05:41 +1000

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,1 +1,13 @@
+let express = require('express');
+let router = express.Router();
+let controller = require('../controllers/controller');
 
+router.post('/', (req,res)=>{
+    controller.postAlbum(req,res);
+});
+
+router.get('/', (req,res) => {
+    controller.getAllAlbums(req,res);
+});
+
+module.exports = router;
