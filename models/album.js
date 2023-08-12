a941b4091b4d2c6ffd9c73c32ea0bf358bc59289
Mitul Tandon <53808940+Mitul22@users.noreply.github.com>
2023-08-12 19:03:08 +1000

    Update album.js

--- models/album.js
+++ models/album.js
@@ -1,1 +1,16 @@
+let client = require('../dbConnection');
+
+let collection;
+
+collection = client.db().collection('Album');
+
+function postAlbum(album,callback) {
+    collection.insertOne(album,callback);
+}
+
+function getAllAlbums(callback){
+    collection.find({}).toArray(callback);
+}
+
+models.exports = {postAlbum,getAllAlbums}
 
