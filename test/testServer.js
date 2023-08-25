20a63ee98a67ed6694ef0ab45b7aed7e8177a7ed
Mitul Tandon <53808940+Mitul22@users.noreply.github.com>
2023-08-25 16:23:49 +1000

    Update testServer.js

--- test/testServer.js
+++ test/testServer.js
@@ -1,28 +1,42 @@
 const {expect} = require("chai");
 const request = require("request");
-const album = require('../server');
+//const album = require('../server');
+url = 'https://localhost:3000/api/album';
+let statusCode = 200;
+let album = {
+    title:'SOS',
+    subTitle:'SZA',
+    descripiton:'This is an R&B album',
+    path:'images/SOS.jpg'
+}
 
 describe('Album GET Testing', function(){
     it('album GET tested',function(){
-        request('https://localhost:3000/api/album',function(a,b,c){
-        console.log('It is sucessfully tested');
+        request({url:url, form:album},function(error,response,c){
+        let bodyObj = JSON.parse(statusCode);
+        expect(bodyObj).to.equal(200);
+        console.log('It is successful !',bodyObj);
         });
     });
 });
 
 describe('Album POST Testing', function(){
     it('album POST tested',function(){
-        request.post('https://localhost:3000/api/album',function(a,b,c){
-        console.log('It is sucessfully tested');
+        request.post({url:url, form:album},function(error,response,c){
+        let bodyObj = JSON.parse(statusCode);
+        expect(bodyObj).to.equal(200);
+        console.log('It is successful !',bodyObj);
         });
     });
 });
 
 describe('Album DELETE Testing', function(){
     it('album DELETE tested',function(){
-        request.delete('https://localhost:3000/api/album',function(a,b,c){
-        console.log('It is sucessfully tested');
+        request.delete({url:url, form:album},function(error,response,c){
+        let bodyObj = JSON.parse(statusCode);
+        expect(bodyObj).to.equal(200);
+        console.log('It is successful !',bodyObj);
+        // done();
         });
     });
 });
-
