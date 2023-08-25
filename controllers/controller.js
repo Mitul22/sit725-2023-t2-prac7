6846a9863be7a91a720de37c44ea5d632c2a9ae7
Mitul Tandon <53808940+Mitul22@users.noreply.github.com>
2023-08-25 16:29:13 +1000

    Update controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,21 +1,31 @@
 let collection = require('../models/album');
 
-const postAlbum  = (req,res) => {
+const postAlbum = (req, res) => {
     let album = req.body;
     collection.postAlbum(album, (err, result) => {
         if (!err) {
-            res.json({statusCode:201, data:result, message:'success'});
+            res.json({
+                statusCode: 201,
+                data: result,
+                message: 'success'
+            });
         }
     });
 }
 
-const getAllAlbums = (req,res) => {
-    collection.getAllAlbums((err,result)=>{
+const getAllAlbums = (req, res) => {
+    collection.getAllAlbums((err, result) => {
         if (!err) {
-             res.json({statusCode:200, data:result, message:'get all albums successful'});
+            res.json({
+                statusCode: 200,
+                data: result,
+                message: 'get all albums successful'
+            });
         }
     });
 }
 
-module.exports = {postAlbum,getAllAlbums}
-
+module.exports = {
+    postAlbum,
+    getAllAlbums
+}
