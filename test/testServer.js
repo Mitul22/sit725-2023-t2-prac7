f0c85ddc62a371c98f6cf0290f6647613504abca
Mitul Tandon <53808940+Mitul22@users.noreply.github.com>
2023-08-25 10:02:07 +1000

    Update testServer.js

--- test/testServer.js
+++ test/testServer.js
@@ -1,1 +1,28 @@
+const {expect} = require("chai");
+const request = require("request");
+const album = require('../server');
+
+describe('Album GET Testing', function(){
+    it('album GET tested',function(){
+        request('https://localhost:3000/api/album',function(a,b,c){
+        console.log('It is sucessfully tested');
+        });
+    });
+});
+
+describe('Album POST Testing', function(){
+    it('album POST tested',function(){
+        request.post('https://localhost:3000/api/album',function(a,b,c){
+        console.log('It is sucessfully tested');
+        });
+    });
+});
+
+describe('Album DELETE Testing', function(){
+    it('album DELETE tested',function(){
+        request.delete('https://localhost:3000/api/album',function(a,b,c){
+        console.log('It is sucessfully tested');
+        });
+    });
+});
 
